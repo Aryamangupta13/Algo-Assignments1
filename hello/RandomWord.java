ca109befdbfc8a888d71297ebef5811adc3f356e
aryaman gupta <aryamangupta747@gmail.com>
2024-09-09 23:44:02 +0530

    alsog assignment 1

---
+++ hello/RandomWord.java
@@ -0,0 +1,12 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord
+{
+    public static void main(String[] args)
+    {
+        String sen=StdIn.readLine();
+        System.out.println(sen);;
+
+    }
+}
